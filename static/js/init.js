bfc279aa394f0d5c73f7afe76c8c07b53d1ff58f
cen1 <cen.is.imba@gmail.com>
2019-05-14 21:50:20 +0200

    reorder on orientation change

--- static/js/init.js
+++ static/js/init.js
@@ -25,6 +25,20 @@
 		console.log("mobile");
 		$("#msgWrapperCol").insertBefore("#userlistBoxCol");
 	}
+
+	var mqOrientation = window.matchMedia("(orientation: portrait)");
+
+	mqOrientation.addListener(function() {
+		console.log("orientation changed");
+		if ($("#loginUIContainer").css("z-index")==2) {
+			console.log("orientationchange 1");
+			$("#msgWrapperCol").insertBefore("#userlistBoxCol");
+		}
+		else {
+			console.log("orientationchange 2");
+			$("#userlistBoxCol").insertBefore("#msgWrapperCol");
+		}
+	});
 });
 
 })(jQuery);
